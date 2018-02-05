13f0afd3f092e1ebb56eb13496dcb8299b3ffb47
mocenas <mocenas@redhat.com>
2018-02-05 13:20:16 +0100

    Added lint co check codestyle.

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const request = require('supertest');
  * Helper do work with (un)deploying applications to openshift.
  * It's statefull, works with the application deployed with deploy() method
  */
-function OpenshiftTestAssistant(){
-    this.retryLimit = 20;
-    this.retryInterval = 5000; // in milliseconds
-    this.ready=false;
-    this.route="";
-    this.config=null;
+function OpenshiftTestAssistant () {
+  this.retryLimit = 20;
+  this.retryInterval = 5000; // in milliseconds
+  this.ready = false;
+  this.route = '';
+  this.config = null;
 }
 
 /**
@@ -18,37 +18,37 @@ function OpenshiftTestAssistant(){
  * @param config Deployment configuration
  * @returns {Promise<>} Promise fulfilled when app is ready, or rejected when when fail
  */
-OpenshiftTestAssistant.prototype.deploy = function(config){
-    let instance=this;
-    this.config=config;
-    return new Promise(function (fulfill, reject){
-        nodeshift.deploy(config)
-        .then(output => { // on success
-            instance.route = "http://" + output.appliedResources.find(val => val.kind === "Route").spec.host;
-            instance.waitForReady(instance.retryLimit)
-            .then(() => {
-                instance.ready=true;
-                fulfill("");
-            }).catch(reason => {
-                reject(reason);
-            });
-        }).catch(reason => { // on failure
+OpenshiftTestAssistant.prototype.deploy = function (config) {
+  const instance = this;
+  this.config = config;
+  return new Promise(function (resolve, reject) {
+    nodeshift.deploy(config)
+      .then(output => { // on success
+        instance.route = 'http://' + output.appliedResources.find(val => val.kind === 'Route').spec.host;
+        instance.waitForReady(instance.retryLimit)
+          .then(() => {
+            instance.ready = true;
+            resolve('');
+          }).catch(reason => {
             reject(reason);
-        });
-    });
+          });
+      }).catch(reason => { // on failure
+        reject(reason);
+      });
+  });
 };
 
-OpenshiftTestAssistant.prototype.getRoute = function(){
-    return this.route;
+OpenshiftTestAssistant.prototype.getRoute = function () {
+  return this.route;
 };
 
-OpenshiftTestAssistant.prototype.isReady = function (){
-    return this.ready;
+OpenshiftTestAssistant.prototype.isReady = function () {
+  return this.ready;
 };
 
-OpenshiftTestAssistant.prototype.undeploy = function(){
-    this.ready=false;
-    return nodeshift.undeploy(this.config);
+OpenshiftTestAssistant.prototype.undeploy = function () {
+  this.ready = false;
+  return nodeshift.undeploy(this.config);
 };
 
 /**
@@ -57,34 +57,31 @@ OpenshiftTestAssistant.prototype.undeploy = function(){
  * @param remainingTries
  * @returns {Promise<>}
  */
-OpenshiftTestAssistant.prototype.waitForReady = function(remainingTries){
-    let instance=this;
-    return new Promise(function (fulfill, reject) {
-        request(instance.route)
-            .get('')
-            .then(response => {
-                if (response.status === 200) { // app ready
-                    fulfill("");
-                }
-                else if (remainingTries > 0) { // app not ready, try another time
-                    setTimeout(function () {
-                        instance.waitForReady(remainingTries - 1)
-                        .then(() => {
-                            fulfill("");
-                        }).catch(reason => {
-                            reject(reason);
-                        });
-                    }, instance.retryInterval);
-                }
-                else { // app not ready, out of tries
-                    reject("Timeout for app deploy");
-                }
-            })
-            .catch(reason => { // failed to connect
+OpenshiftTestAssistant.prototype.waitForReady = function (remainingTries) {
+  const instance = this;
+  return new Promise(function (resolve, reject) {
+    request(instance.route)
+      .get('')
+      .then(response => {
+        if (response.status === 200) { // app ready
+          resolve('');
+        } else if (remainingTries > 0) { // app not ready, try another time
+          setTimeout(function () {
+            instance.waitForReady(remainingTries - 1)
+              .then(() => {
+                resolve('');
+              }).catch(reason => {
                 reject(reason);
-            })
-    });
+              });
+          }, instance.retryInterval);
+        } else { // app not ready, out of tries
+          reject(new Error('Timeout for app deploy'));
+        }
+      })
+      .catch(reason => { // failed to connect
+        reject(reason);
+      });
+  });
 };
 
 module.exports = OpenshiftTestAssistant;
-
